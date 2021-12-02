eee0cb1990ff8147c95bd7a55a53903c7b739506
Charles Rudder <crudder@citiesthatwork.com>
2021-12-02 18:46:56 -0500

    outline of tasks for package_switcher script

--- {{cookiecutter.project_name}}/scripts/package_switcher.py
+++ {{cookiecutter.project_name}}/scripts/package_switcher.py
@@ -1,3 +1,24 @@
 # TODO: setup this script to checkout the correct branch and create a new project branch
+import git
+from pathlib import Path
+from check_package_deps import Environment
 
+PROJECT_DIR = Path(__file__).parent.parent
+PACKAGES_DIR = PROJECT_DIR.parent / 'packages'
+
+# assumes all branches of our packages will be branches off of whatever is provided here
 INITIAL_BRANCH = {{cookiecutter.package_base_branches}}
+PROJECT_NAME = {{cookiecutter.project_name}}
+ENVIRONMENT_YML = Path(PROJECT_DIR, 'environment.yml')
+
+environment = Environment(ENVIRONMENT_YML)
+# identify local packages in the environment
+
+
+"""
+1) check to see if the repo is already local
+2) if not, clone it
+3) if it is, set branch to (DEV_INIT)
+4) pull the latest changes
+6) create a new branch from the package with the project name as the branch [dev-init-project-name]
+"""
